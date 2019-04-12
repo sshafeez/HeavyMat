4978cd0e789164849f0d0b8f5ec3caca6ecb82f4
ozanaktay <49240269+ozanaktay@users.noreply.github.com>
2019-04-12 13:08:43 -0400

    double instead of unsigned

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    vector<vector<unsigned> > vec;
+    vector<vector<double> > vec;
     image_read("usFlag.png", 425, 276, vec);
     image_write("image.png", vec);
     return 0;
