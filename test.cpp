0b9eafb68a180ab78a12e720bf9800619187c964
sshafeez <sshafeez@umich.edu>
2019-04-19 13:54:44 -0400

    forcing thread ID's to print first

--- test.cpp
+++ test.cpp
@@ -16,13 +16,14 @@ int main() {
 	heavy_matrix G(0, 0);
 	heavy_matrix B(0, 0);
 
-	image_read("docs.png", R.grid, G.grid, B.grid);
+	image_read("M.png", R.grid, G.grid, B.grid);
+	printLock.lock();
 	thread t1(process,ref(R));
 	thread t2(process,ref(G));
 	thread t3(process,ref(B));
-	cout<<"R: "<<t1.get_id()<< " G: "<<t2.get_id()<<" B: "<<t3.get_id()<<endl;
+	cout<<"R: "<<t1.get_id()<< " G: "<<t2.get_id()<<" B: "<<t3.get_id()<<endl; printLock.unlock();
 	t1.join(); t2.join(); t3.join();
-	image_write("docs2.png", R.grid, G.grid, B.grid);
+	image_write("M2.png", R.grid, G.grid, B.grid);
 
     return 0;
 }
